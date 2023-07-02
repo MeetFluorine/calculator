0224dff16257521740d102331790ae3be64d07cd
Chandan Yadav <103582224+MeetFluorine@users.noreply.github.com>
2023-07-02 18:27:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
-function appendValue(value) {
-    var result = document.getElementById("d");
-    result.value += value;
-  }
-  
-  function calculate() {
-    var result = document.getElementById("d");
-    result.value = eval(result.value);
-  }
-  
-  function clearResult() {
-    var result = document.getElementById("d");
-    result.value = "";
-  }
-  
-  function clearOne(){
+let result = document.getElementById("d");
+function appendValue(value){
+    
+    result.value+=value;
+}
+function calculate(){
+    result.value=eval(result.value);
+}
+function clearResult(){
+    result.value="";
+}
+function clearOne(){
     var result= document.getElementById("d").value;
      result= result.substr(0,result.length-1);
      document.getElementById("d").value= result;
   } 
 
-window.addEventListener('keyup',function(event){
-  var result = document.getElementById("d");
-  if(event.key >=0 && event.key<=9 || event.key=="+" || event.key=="-" || event.key=="*" || event.key=="/" || event.key=="%" || event.key==".")
-  {
-  result.value += event.key;
-  }
 
-  if(event.key=="Enter"){
+window.addEventListener('keydown',function(event){
+    if(event.key >=0 && event.key<=9 || event.key=="+" || event.key=="-" || event.key=="*" || event.key=="/" || event.key=="%" || event.key=="."){
+      result.value += event.key;
+  }
+  else if(event.key =="Enter"){
     calculate();
   }
-
-  if(event.key=="Backspace"){
+  else if(event.key == "Backspace"){
     clearOne();
   }
-  if(event.key=="Delete"){
+  else if(event.key=="Delete"){
     clearResult();
   }
-
-});
-
- 
+})
